1b2b7a40f23714e9afcabfbf7966e1dd955375dc
GabssssMes <messner92@gmail.com>
2023-07-20 12:21:54 +0200

    change fonts on home-tab

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,9 +7,11 @@ const Home = () => {
   return (
     <div className={style.home}>
       <div className={style.Slogan}>
-        <p>"The time you</p>
-        <p> ENJOY wasting </p>
-        <p>is not WASTED time"</p>
+        <p className={style.saying}>
+          "Everyone looks at your watch and it represents who you are, your
+          values and your personal style"
+        </p>
+        <p className={style.origin}>--- Kobe Bryant ---</p>
       </div>
       <Button variant="contained" component={Link} to="/products">
         Get your Watch here
